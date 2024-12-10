c4ddfdc0095903aba54f1a4378d575c5f2f9369f
Jonathan Zea <jonathan.a.zea@ieee.org>
2024-12-09 23:54:50 -0500

    :sparkles: upload working code using yolo 80cats

---
+++ android_app/android_app/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -0,0 +1,11 @@
+package com.surendramaran.yolov8tflite
+
+object Constants {
+    //const val MODEL_PATH = "model_neso613.tflite"
+    const val MODEL_PATH = "yolov8n_float16.tflite"
+    // const val MODEL_PATH = "yolov5s-fp16.tflite"
+    //const val MODEL_PATH = "model_forks.tflite"
+    // const val LABELS_PATH = "labels.txt" // forks
+    const val LABELS_PATH = "labels_v8.txt" // forks
+}
+
